fc5ad1a4db9c34b4cf6a2c1b4f3c5ffee21181ba
Mr-SimpleEditor <Mr-SimpleEditor@protonmail.com>
2022-07-24 01:47:34 +0530

    Just added

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("I'm from javascript.")
+var a = 45;
+var b = "Sumit";
+var c = null
+var d = undefined
+
+
+console.log(b)
+
+// block
+// Use let instead var. let scope is in block level.
+// var can be declared many times. But let can be declared once and can be modified.
+
+{
+
+	let b = "Patel"
+	console.log(b)
+
+}
+
+
+console.log(b)
+
+
+const author = "Sumit"
+console.log("Our Author is : " + author)
