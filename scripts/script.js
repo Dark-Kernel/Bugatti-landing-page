35e1ffa4f374ecf8fe3601a300c20577ec6db97e
Mr-SimpleEditor <Mr-SimpleEditor@protonmail.com>
2022-07-25 02:46:04 +0530

    added hamburger menu only

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,4 @@
-console.log("I'm from javascript.")
-var a = 45;
-var b = "Sumit";
-var c = null
-var d = undefined
 
-
-console.log(b)
-
-// block
-// Use let instead var. let scope is in block level.
-// var can be declared many times. But let can be declared once and can be modified.
-
-{
-
-	let b = "Patel"
-	console.log(b)
-
+function myFunction(x) {
+	  x.classList.toggle("change");
 }
-
-
-console.log(b)
-
-
-const author = "Sumit"
-console.log("Our Author is : " + author)
